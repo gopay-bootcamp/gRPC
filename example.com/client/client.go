64665935fb092b38043c5d93e21aeb48406ffe39
jaimin.rathod <3331-jaimin.rathod@users.noreply.source.golabs.io>
2020-07-30 09:57:41 +0530

    Added client and server code for gRPC

---
+++ example.com/client/client.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	proto "gRPC_demo/example.com/services"
+	"google.golang.org/grpc"
+	"io"
+)
+func main() {
+	conn,_ :=grpc.Dial("localhost:8000",grpc.WithInsecure())
+	client:=proto.NewBooksServicesClient(conn)
+	addBook(client,"My first book","abc")
+	addBook(client,"My second book","abc")
+	getBooks(client)
+	getBook(client,2)
+	getBook(client, 1)
+	getBook(client,3)
+	updateBook(client,2,"My second book(updated)","abc(xyz)")
+	getBook(client,2)
+	deleteBook(client,1)
+	getBook(client,1)
+	deleteBook(client,1)
+
+}
+
+func getBooks(client proto.BooksServicesClient) {
+	request:=proto.RequestForGetBooks{}
+	responseStream,_:=client.GetBooks(context.Background(),&request)
+
+	for {
+		book,err := responseStream.Recv()
+		if err == io.EOF {
+			break
+		}
+		fmt.Printf("ID = %v, Book Name = %v, Author = %v\n",book.ID,book.BookName,book.AuthorName)
+	}
+
+}
+func addBook(client proto.BooksServicesClient,bookName string,authorName string) {
+	request:=proto.RequestForAddBook{BookName: bookName,AuthorName: authorName}
+	response,_:=client.AddBook(context.Background(),&request)
+	fmt.Printf("Book added with ID = %v, Book Name = %v, Author = %v\n", response.ID, response.BookName, response.AuthorName)
+}
+func getBook(client proto.BooksServicesClient,id int64) {
+	request:=proto.RequestForGetBook{ID: id}
+	response,_:=client.GetBook(context.Background(),&request)
+	fmt.Printf("ID = %v, Book Name = %v, Author = %v\n", response.ID, response.BookName, response.AuthorName)
+}
+func updateBook(client proto.BooksServicesClient,id int64,bookName string,authorName string) {
+	request:=proto.RequestForUpdateBook{ID:id,BookName: bookName,AuthorName: authorName}
+	response,_:=client.UpdateBook(context.Background(),&request)
+	fmt.Printf("Book updated with ID = %v, Book Name = %v, Author = %v\n", response.ID, response.BookName, response.AuthorName)
+}
+func deleteBook(client proto.BooksServicesClient,id int64) {
+	request := proto.RequestForDeleteBook{ID: id}
+	response, _ := client.DeleteBook(context.Background(), &request)
+	if response.Flag==1 {
+		fmt.Printf("Book deleted with id = %v\n",id)
+	} else {
+		fmt.Printf("book with given id not exists\n")
+	}
+
+}
---
+++ example.com/server/server.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"net"
+	proto "gRPC_demo/example.com/services"
+)
+
+type server struct {
+}
+
+type book struct {
+	ID int64
+	BookName string
+	AuthorName string
+}
+
+var books []book
+
+var id int64=1
+func main()  {
+	listener,err:=net.Listen("tcp",":8000")
+
+	srvr:=grpc.NewServer()
+
+	proto.RegisterBooksServicesServer(srvr,&server{})
+
+	if err==nil {
+		srvr.Serve(listener)
+	}
+}
+
+
+func (s *server) AddBook(ctx context.Context,request *proto.RequestForAddBook) (*proto.Response,error) {
+	var book book
+	book.ID=id
+	book.BookName=request.BookName
+	book.AuthorName=request.AuthorName
+	books=append(books,book)
+	id=id+1
+	return &proto.Response{ID: book.ID, BookName: book.BookName, AuthorName: book.AuthorName}, nil
+}
+
+func (s *server) GetBook(ctx context.Context,request *proto.RequestForGetBook) (*proto.Response,error) {
+	requestedBookId:=request.ID
+	for _, item := range books {
+		if item.ID == requestedBookId {
+
+			return &proto.Response{ID: item.ID,BookName: item.BookName,AuthorName: item.AuthorName},nil
+		}
+	}
+	return &proto.Response{ID: 0, BookName: "", AuthorName: ""}, nil
+}
+func (s *server) GetBooks(request *proto.RequestForGetBooks,stream proto.BooksServices_GetBooksServer) error {
+	for _,book := range books {
+		stream.Send(&proto.Response{ID: book.ID,BookName: book.BookName,AuthorName: book.AuthorName})
+	}
+	return nil
+}
+
+func (s *server) UpdateBook(ctx context.Context,request *proto.RequestForUpdateBook) (*proto.Response,error) {
+	requestedUpdateId:=request.ID
+	for index, item := range books {
+		if item.ID == requestedUpdateId {
+			books = append(books[:index], books[index+1:]...)
+			var book book
+			book.ID = requestedUpdateId
+			book.BookName=request.BookName
+			book.AuthorName=request.AuthorName
+			books = append(books, book)
+			return &proto.Response{ID: book.ID,BookName: book.BookName,AuthorName: book.AuthorName},nil
+		}
+	}
+	return &proto.Response{ID: 0, BookName: "", AuthorName: ""}, nil
+}
+
+func (s *server) DeleteBook(ctx context.Context,request *proto.RequestForDeleteBook) (*proto.DeleteResponse,error) {
+	for index, item := range books {
+		if item.ID == request.ID {
+			books = append(books[:index], books[index+1:]...)
+			return &proto.DeleteResponse{Flag: 1},nil
+		}
+	}
+	return &proto.DeleteResponse{Flag: 0}, nil
+}
+
+
+
+
+
+
+
+
+
