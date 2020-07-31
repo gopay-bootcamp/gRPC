11ccfb20e01173cfbf7c3c53d1ddd6b7b27520b3
jaimin.rathod <3331-jaimin.rathod@users.noreply.source.golabs.io>
2020-07-31 12:42:58 +0530

    Minor bug fixes

--- example.com/client/client.go
+++ example.com/client/client.go
@@ -8,7 +8,7 @@ import (
 	"io"
 )
 func main() {
-	conn,_ :=grpc.Dial("localhost:8000",grpc.WithInsecure())
+	conn,_:=grpc.Dial("localhost:8000",grpc.WithInsecure())
 	client:=proto.NewBooksServicesClient(conn)
 	addBook(client,"My first book","abc")
 	addBook(client,"My second book","abc")
@@ -26,8 +26,11 @@ func main() {
 
 func getBooks(client proto.BooksServicesClient) {
 	request:=proto.RequestForGetBooks{}
-	responseStream,_:=client.GetBooks(context.Background(),&request)
-
+	responseStream,error:=client.GetBooks(context.Background(),&request)
+ 	if error!=nil {
+ 		fmt.Println(error.Error())
+ 		return
+	}
 	for {
 		book,err := responseStream.Recv()
 		if err == io.EOF {
@@ -39,22 +42,38 @@ func getBooks(client proto.BooksServicesClient) {
 }
 func addBook(client proto.BooksServicesClient,bookName string,authorName string) {
 	request:=proto.RequestForAddBook{BookName: bookName,AuthorName: authorName}
-	response,_:=client.AddBook(context.Background(),&request)
+	response,error:=client.AddBook(context.Background(),&request)
+	if error!=nil {
+		fmt.Println(error.Error())
+		return
+	}
 	fmt.Printf("Book added with ID = %v, Book Name = %v, Author = %v\n", response.ID, response.BookName, response.AuthorName)
 }
 func getBook(client proto.BooksServicesClient,id int64) {
 	request:=proto.RequestForGetBook{ID: id}
-	response,_:=client.GetBook(context.Background(),&request)
+	response,error:=client.GetBook(context.Background(),&request)
+	if error!=nil {
+		fmt.Println(error.Error())
+		return
+	}
 	fmt.Printf("ID = %v, Book Name = %v, Author = %v\n", response.ID, response.BookName, response.AuthorName)
 }
 func updateBook(client proto.BooksServicesClient,id int64,bookName string,authorName string) {
 	request:=proto.RequestForUpdateBook{ID:id,BookName: bookName,AuthorName: authorName}
-	response,_:=client.UpdateBook(context.Background(),&request)
+	response,error:=client.UpdateBook(context.Background(),&request)
+	if error!=nil {
+		fmt.Println(error.Error())
+		return
+	}
 	fmt.Printf("Book updated with ID = %v, Book Name = %v, Author = %v\n", response.ID, response.BookName, response.AuthorName)
 }
 func deleteBook(client proto.BooksServicesClient,id int64) {
 	request := proto.RequestForDeleteBook{ID: id}
-	response, _ := client.DeleteBook(context.Background(), &request)
+	response, error := client.DeleteBook(context.Background(), &request)
+	if error!=nil {
+		fmt.Println(error.Error())
+		return
+	}
 	if response.Flag==1 {
 		fmt.Printf("Book deleted with id = %v\n",id)
 	} else {
