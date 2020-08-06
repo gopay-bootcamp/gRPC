f5d6b0a6f835ddb75f192a0434a8e15e721f9544
jaimin.rathod <3331-jaimin.rathod@users.noreply.source.golabs.io>
2020-08-06 12:40:14 +0530

    Some bug fixes

--- example.com/server/server_test.go
+++ example.com/server/server_test.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"context"
+	"gRPC/example.com/Model"
 	proto "gRPC/example.com/services"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/test/bufconn"
 	"io"
 	"log"
 	"net"
-
+	"gRPC/example.com/bookUtil"
 	"testing"
 )
 
@@ -53,8 +54,8 @@ func bufDialer(context.Context, string) (net.Conn, error) {
 
 
 func testGetBooks(t *testing.T, client proto.BooksServicesClient) {
-	books = append(books, book{ID: 1, BookName: "My first book", AuthorName: "abc"})
-	books = append(books, book{ID: 2, BookName: "My second book", AuthorName: "abc"})
+	bookUtil.Books = append(bookUtil.Books, Model.Book{ID: 1, BookName: "My first book", AuthorName: "abc"})
+	bookUtil.Books = append(bookUtil.Books, Model.Book{ID: 2, BookName: "My second book", AuthorName: "abc"})
 
 	request:=proto.RequestForGetBooks{}
 	responseStream,err:=client.GetBooks(context.Background(),&request)
@@ -76,7 +77,7 @@ func testGetBooks(t *testing.T, client proto.BooksServicesClient) {
 	if err3!=io.EOF {
 		t.Errorf("GetBooks failed")
 	}
-	books = nil
+	bookUtil.Books = nil
 }
 
 func testCreateBook(t *testing.T, client proto.BooksServicesClient) {
@@ -88,15 +89,15 @@ func testCreateBook(t *testing.T, client proto.BooksServicesClient) {
 	if response.ID!=1 {
 		t.Errorf("CreateBook failed")
 	}
-	if books[0].ID!=1 || books[0].BookName!="My first book" || books[0].AuthorName!="abc"  {
+	if bookUtil.Books[0].ID!=1 || bookUtil.Books[0].BookName!="My first book" || bookUtil.Books[0].AuthorName!="abc"  {
 		t.Errorf("CreateBook failed")
 	}
-	books = nil
+	bookUtil.Books = nil
 }
 
 func testGetBook(t *testing.T, client proto.BooksServicesClient) {
-	books = append(books, book{ID: 1, BookName: "My first book", AuthorName: "abc"})
-	books = append(books, book{ID: 2, BookName: "My second book", AuthorName: "xyz"})
+	bookUtil.Books = append(bookUtil.Books, Model.Book{ID: 1, BookName: "My first book", AuthorName: "abc"})
+	bookUtil.Books = append(bookUtil.Books, Model.Book{ID: 2, BookName: "My second book", AuthorName: "xyz"})
 
 	request:=proto.RequestForGetBook{ID: 1}
 	response,err:=client.GetBook(context.Background(),&request)
@@ -115,31 +116,31 @@ func testGetBook(t *testing.T, client proto.BooksServicesClient) {
 	if response.ID!=2 || response.BookName!="My second book" || response.AuthorName!="xyz" {
 		t.Errorf("GetBook failed")
 	}
-	books=nil
+	bookUtil.Books=nil
 }
 
 func testUpdateBook(t *testing.T, client proto.BooksServicesClient) {
-	books = append(books, book{ID: 1, BookName: "My first book", AuthorName: "abc"})
-	books = append(books, book{ID: 2, BookName: "My second book", AuthorName: "abc"})
+	bookUtil.Books = append(bookUtil.Books, Model.Book{ID: 1, BookName: "My first book", AuthorName: "abc"})
+	bookUtil.Books = append(bookUtil.Books, Model.Book{ID: 2, BookName: "My second book", AuthorName: "abc"})
 
 	request:=proto.RequestForUpdateBook{ID: 2,BookName: "My updated second book",AuthorName: "abc(xyz)"}
 	response,err:=client.UpdateBook(context.Background(),&request)
 	if err!=nil || response==nil{
 		t.Errorf("UpdateBook failed")
 	}
-	if books[0].ID!=1 || books[0].BookName!="My first book" || books[0].AuthorName!="abc"  {
+	if bookUtil.Books[0].ID!=1 || bookUtil.Books[0].BookName!="My first book" || bookUtil.Books[0].AuthorName!="abc"  {
 		t.Errorf("UpdateBook failed")
 	}
-	if books[1].ID!=2 || books[1].BookName!="My updated second book" || books[1].AuthorName!="abc(xyz)"  {
+	if bookUtil.Books[1].ID!=2 || bookUtil.Books[1].BookName!="My updated second book" || bookUtil.Books[1].AuthorName!="abc(xyz)"  {
 		t.Errorf("UpdateBook failed")
 	}
-	books=nil
+	bookUtil.Books=nil
 }
 
 func testDeleteBook(t *testing.T, client proto.BooksServicesClient) {
-	books = append(books, book{ID: 1, BookName: "My first book", AuthorName: "abc"})
-	books = append(books, book{ID: 2, BookName: "My second book", AuthorName: "abc"})
-	books = append(books, book{ID: 3, BookName: "My third book", AuthorName: "abc"})
+	bookUtil.Books = append(bookUtil.Books, Model.Book{ID: 1, BookName: "My first book", AuthorName: "abc"})
+	bookUtil.Books = append(bookUtil.Books, Model.Book{ID: 2, BookName: "My second book", AuthorName: "abc"})
+	bookUtil.Books = append(bookUtil.Books, Model.Book{ID: 3, BookName: "My third book", AuthorName: "abc"})
 
 	request:=proto.RequestForDeleteBook{ID: 2}
 	response,err:=client.DeleteBook(context.Background(),&request)
@@ -149,13 +150,13 @@ func testDeleteBook(t *testing.T, client proto.BooksServicesClient) {
 	if response.Flag!=1 {
 		t.Errorf("DeleteBook failed")
 	}
-	if books[0].ID!=1 || books[0].BookName!="My first book" || books[0].AuthorName!="abc"  {
+	if bookUtil.Books[0].ID!=1 || bookUtil.Books[0].BookName!="My first book" || bookUtil.Books[0].AuthorName!="abc"  {
 		t.Errorf("DeleteBook failed")
 	}
-	if books[1].ID!=3 || books[1].BookName!="My third book" || books[1].AuthorName!="abc"  {
+	if bookUtil.Books[1].ID!=3 || bookUtil.Books[1].BookName!="My third book" || bookUtil.Books[1].AuthorName!="abc"  {
 		t.Errorf("DeleteBook failed")
 	}
-	books=nil
+	bookUtil.Books=nil
 
 }
 
